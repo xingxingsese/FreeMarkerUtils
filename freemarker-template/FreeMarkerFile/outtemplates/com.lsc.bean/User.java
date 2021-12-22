05a1f42328c2932afd9e0a41d56fbb225c380204
lsc <zuihou098@qq.com>
2021-12-22 23:33:03 +0800

    ces

---
+++ freemarker-template/FreeMarkerFile/outtemplates/com.lsc.bean/User.java
@@ -0,0 +1,30 @@
+package com.lsc.bean;
+
+
+public class User {
+
+    /**
+    * id
+    */
+    private int id;
+    /**
+    * name
+    */
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public Integer getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
